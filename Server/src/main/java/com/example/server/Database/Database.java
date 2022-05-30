b2a3829136c4c909a80968f4450ee1f0d9b830a5
rezamosavi <rezamosavi@rezamosavis-MacBook-Air.local>
2022-05-30 23:30:29 +0430

    add database functions

--- Server/src/main/java/com/example/server/Database/Database.java
+++ Server/src/main/java/com/example/server/Database/Database.java
@@ -1,4 +1,27 @@
 package com.example.server.Database;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.Collection;
+
 public abstract class Database {
+    private MongoClient mongoClient = new MongoClient("localhost",27017);
+    private MongoDatabase database = mongoClient.getDatabase("Divar");
+    private MongoCollection<Document> collection;
+    private Document document = new Document();
+
+    public void setCollection(String collectionName){
+        collection = database.getCollection(collectionName);
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+
+    public void connectToDatabase(){
+        
+    }
 }
