04f8bb21bbc8ab442770a6bebcce068fcba3642d
Mohammad Rouintan <mdrn1381@gmail.com>
2022-05-30 23:10:24 +0430

    add mongodb to module-info.java

--- Server/src/main/java/module-info.java
+++ Server/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.server {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires mongo.java.driver;
 
     opens com.example.server to javafx.fxml;
     exports com.example.server;
