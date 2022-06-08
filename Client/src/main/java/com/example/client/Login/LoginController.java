8687850f27120fccb3d72588a02bfff22a4fbad0
rezamosavi <rezamosavi@rezamosavis-MacBook-Air.local>
2022-06-09 00:13:52 +0430

    switch pane in login

--- Client/src/main/java/com/example/client/Login/LoginController.java
+++ Client/src/main/java/com/example/client/Login/LoginController.java
@@ -1,6 +1,7 @@
 package com.example.client.Login;
 
 import com.example.client.Main;
+import com.example.client.socket.Connect;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,12 +14,20 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginController {
     @FXML
     private Button loginButtonID;
 
-    private String phoneNumber = "0000000";
+    private String phoneNumber;
+
+    @FXML
+    private Button ReCodeButtonID;
+
+    @FXML
+    private Label counterID;
 
     @FXML
     private void loginOnFirstPage(ActionEvent e){
@@ -44,15 +53,47 @@ public class LoginController {
      * TODO
      */
     private void getInputNumberField(){
-        phoneNumber = inputPhoneNumberCodeID.getText();
-        System.out.println(phoneNumber);
+
     }
 
     @FXML
     private Button sendButtonPhoneID;
 
     @FXML
-    private void sendButtonPhoneNumber(){}
+    private Label test;
+
+    @FXML
+    private void sendButtonPhoneNumber(ActionEvent e){
+
+        phoneNumber = "";
+
+        Pattern pattern = Pattern.compile("09[0-9]{9}");
+        Matcher matcher = pattern.matcher(inputPhoneNumberIDField.getText());
+
+        test.setText(String.valueOf(matcher.matches()));
+
+
+        if(matcher.matches()){
+
+            new Connect("localhost" ,"localhost", inputPhoneNumberIDField.getText());
+
+            try{
+                Parent layout = FXMLLoader.load(Main.class.getResource("Code.fxml"));
+                Stage stage = (Stage) ((Node)e.getSource()).getScene().getWindow();
+                Scene scene = new Scene(layout,800,600);
+                stage.setTitle("Dashboard");
+                stage.setMinHeight(600);
+                stage.setMinWidth(800);
+                stage.setScene(scene);
+                stage.show();
+
+            }catch (IOException ex){
+                ex.printStackTrace();
+            }
+
+        }
+
+    }
 
     @FXML
     private Label label1CodeFile = null;
--- Server/src/main/java/module-info.java
+++ Server/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.server {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.example.server to javafx.fxml;
     exports com.example.server;
 }
