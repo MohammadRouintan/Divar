058550a4066ec8e878f8a9effa634d2199df373f
Abolfazl <abolfazl30not30@gmail.com>
2022-06-08 23:54:21 +0430

    change the font

--- Client/src/main/java/com/example/client/Main.java
+++ Client/src/main/java/com/example/client/Main.java
@@ -8,7 +8,6 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("entrance.fxml"));
