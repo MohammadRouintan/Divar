bafcce9d93be04d24af83dc07cf79a89bba93ad2
rezamosavi <rezamosavi@rezamosavis-MacBook-Air.local>
2022-06-22 01:44:09 +0430

    add controller class

---
+++ Client/src/main/java/com/example/client/Dashboard/MyDivarPages/ProfileController.java
@@ -0,0 +1,32 @@
+package com.example.client.Dashboard.MyDivarPages;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class ProfileController {
+
+    @FXML
+    private ImageView profileImage;
+
+    @FXML
+    private TextField textFiledCity;
+
+    @FXML
+    private TextField textFiledFirstName;
+
+    @FXML
+    private TextField textFiledLastName;
+
+    @FXML
+    void uploadData(ActionEvent event) {
+
+    }
+
+    @FXML
+    void uploadImageProfile(ActionEvent event) {
+
+    }
+
+}
