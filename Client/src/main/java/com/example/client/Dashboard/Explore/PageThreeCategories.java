6656e81ec85c2de69efa29dd806175e6bca5601d
Abolfazl <abolfazl30not30@gmail.com>
2022-06-25 13:47:24 +0430

    make categories page

---
+++ Client/src/main/java/com/example/client/Dashboard/Explore/PageThreeCategories.java
@@ -0,0 +1,129 @@
+package com.example.client.Dashboard.Explore;
+
+import com.example.client.Dashboard.Posts.NewPage;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Pagination;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PageThreeCategories {
+    private String nameCat;
+    private VBox mainVBox;
+    private VBox vBox;
+    private List<VBox> PagesList = new ArrayList<VBox>();
+
+    public PageThreeCategories(Parent parent,String nameCat){
+        this.mainVBox = (VBox)parent;
+        this.vBox = (VBox) mainVBox.getChildren().get(0);
+        this.nameCat = nameCat;
+        addPagination();
+    }
+
+    private void addPagination(){
+        this.mainVBox.getChildren().clear();
+        VBox threeCategories = new VBox();
+        Label TitleLabel = new Label(nameCat);
+        Button back = new Button("Back");
+        HBox tophBox = new HBox();
+
+        Pagination pagination = new Pagination();
+        pagination.setPrefWidth(570);
+        pagination.setPrefHeight(1200);
+        //test the show of post
+
+        ArrayList<JSONObject> post = new ArrayList<>();
+        ArrayList<String> imageName = new ArrayList<>();
+        imageName.add("1");
+        imageName.add("2");
+        ArrayList<String> columnName = new ArrayList<>();
+        columnName.add("Salam");
+        columnName.add("Khobi");
+
+        ArrayList<String> columnValue = new ArrayList<>();
+        columnValue.add("dddddd");
+        columnValue.add("bbbbbb");
+
+        for (int i = 0; i < 7; i++) {
+            JSONObject post1 = new JSONObject();
+            post1.put("title", "salam");
+            post1.put("price", "14000");
+            post1.put("time", "5");
+            post1.put("image1", "/postImage/1.jpg");
+            post1.put("imageName",imageName);
+            post1.put("columnName",columnName);
+            post1.put("columnValue",columnValue);
+            post1.put("bio","I have a scene showing 3 images, and I want each of them to take a third of the width of the scene. " +
+                    "From now, I have made 3 Pane of each 30% of it, it works. But in those Pane," +
+                    " I can't make my ImageView use only the width of the Pane.");
+            post1.put("agreement", true);
+            post1.put("exchange", true);
+            post1.put("auction", false);
+            post.add(post1);
+        }
+
+        VBox vBox = new VBox();
+        NewPage newPage = new NewPage(post,vBox, "BookmarkedAds");
+        PagesList.add(newPage.getPage());
+        pagination.setPageFactory(this::CreatePage);
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setPrefWidth(570);
+        scrollPane.setPrefHeight(600);
+        scrollPane.setContent(pagination);
+
+        tophBox.getChildren().addAll(back,TitleLabel);
+
+        threeCategories.getChildren().addAll(tophBox,scrollPane);
+        this.mainVBox.getChildren().add(threeCategories);
+
+        back.setOnAction(event -> {
+            mainVBox.getChildren().clear();
+            mainVBox.getChildren().add(this.vBox);
+        });
+    }
+
+    private Node CreatePage(int pageIndex) {
+//        ArrayList<JSONObject> userPosts = new ArrayList<>();
+//        for (int i = (pagination.getCurrentPageIndex() - 1) * 8; i < pagination.getCurrentPageIndex() * 8; i++) {
+//            userPosts.add(new JSONObject(GetInfo.getUserPosts().get(i)));
+//        }
+        ArrayList<JSONObject> post = new ArrayList<>();
+        ArrayList<String> imageName = new ArrayList<>();
+        imageName.add("1");
+        imageName.add("2");
+        ArrayList<String> columnName = new ArrayList<>();
+        columnName.add("Salam");
+        columnName.add("Khobi");
+
+        ArrayList<String> columnValue = new ArrayList<>();
+        columnValue.add("dddddd");
+        columnValue.add("bbbbbb");
+
+        for (int i = 0; i < 7; i++) {
+            JSONObject post1 = new JSONObject();
+            post1.put("title", "salam");
+            post1.put("price", "14000");
+            post1.put("time", "5");
+            post1.put("image1", "/postImage/1.jpg");
+            post1.put("imageName",imageName);
+            post1.put("columnName",columnName);
+            post1.put("columnValue",columnValue);
+            post1.put("bio","I have a scene showing 3 images, and I want each of them to take a third of the width of the scene. " +
+                    "From now, I have made 3 Pane of each 30% of it, it works. But in those Pane," +
+                    " I can't make my ImageView use only the width of the Pane.");
+            post.add(post1);
+        }
+        VBox vBox = new VBox();
+        NewPage newPage = new NewPage(post,vBox,"Categories");
+        PagesList.add(newPage.getPage());
+        return PagesList.get(pageIndex);
+    }
+
+}
--- Client/src/main/java/com/example/client/Dashboard/Explore/PageTwoCategories.java
+++ Client/src/main/java/com/example/client/Dashboard/Explore/PageTwoCategories.java
@@ -39,251 +39,195 @@ public class PageTwoCategories {
         vBox.getChildren().add(showAll);
         switch (nameCat) {
             case "Estate":
-                Button ResidentialSales = new Button("ResidentialSales");
-                ResidentialSales.setPrefWidth(500);
-                ResidentialSales.setPrefHeight(44);
-                Button ResidentialRent = new Button("ResidentialRent");
-                ResidentialRent.setPrefWidth(500);
-                ResidentialRent.setPrefHeight(44);
-                Button OfficeSales = new Button("OfficeSales");
-                OfficeSales.setPrefWidth(500);
-                OfficeSales.setPrefHeight(44);
-                Button OfficeRent = new Button("OfficeRent");
-                OfficeRent.setPrefWidth(500);
-                OfficeRent.setPrefHeight(44);
-                Button ShortTermRent = new Button("ShortTermRent");
-                ShortTermRent.setPrefWidth(500);
-                ShortTermRent.setPrefHeight(44);
-                Button ConstructionProject = new Button("ConstructionProject");
-                ConstructionProject.setPrefWidth(500);
-                ConstructionProject.setPrefHeight(44);
+                Button ResidentialSales = makeButton("ResidentialSales");
+
+                Button ResidentialRent = makeButton("ResidentialRent");
+
+                Button OfficeSales = makeButton("OfficeSales");
+
+                Button OfficeRent = makeButton("OfficeRent");
+
+                Button ShortTermRent = makeButton("ShortTermRent");
+
+                Button ConstructionProject = makeButton("ConstructionProject");
+
                 vBox.getChildren().addAll(ResidentialSales, ResidentialRent, OfficeSales, OfficeRent, ShortTermRent, ConstructionProject);
                 break;
             case "Vehicles":
-                Button Car = new Button("Car");
-                Car.setPrefWidth(500);
-                Car.setPrefHeight(44);
-                Button CarSparePartsAndAccessories = new Button("CarSparePartsAndAccessories");
-                CarSparePartsAndAccessories.setPrefWidth(500);
-                CarSparePartsAndAccessories.setPrefHeight(44);
-                Button MotorcyclesAndAccessories = new Button("MotorcyclesAndAccessories");
-                MotorcyclesAndAccessories.setPrefWidth(500);
-                MotorcyclesAndAccessories.setPrefHeight(44);
-                Button BoatsAndAccessories = new Button("BoatsAndAccessories");
-                BoatsAndAccessories.setPrefWidth(500);
-                BoatsAndAccessories.setPrefHeight(44);
+                Button Car = makeButton("Car");
+
+                Button CarSparePartsAndAccessories = makeButton("CarSparePartsAndAccessories");
+;
+                Button MotorcyclesAndAccessories = makeButton("MotorcyclesAndAccessories");
+
+                Button BoatsAndAccessories = makeButton("BoatsAndAccessories");
+
                 vBox.getChildren().addAll(Car, CarSparePartsAndAccessories, MotorcyclesAndAccessories, BoatsAndAccessories);
                 break;
+
             case "Digital Goods":
-                Button MobileAndTablet = new Button("MobileAndTablet");
-                MobileAndTablet.setPrefWidth(500);
-                MobileAndTablet.setPrefHeight(44);
-                Button PC = new Button("PC");
-                PC.setPrefWidth(500);
-                PC.setPrefHeight(44);
-                Button Console = new Button("Console");
-                Console.setPrefWidth(500);
-                Console.setPrefHeight(44);
-                Button VideoAndAudio = new Button("VideoAndAudio");
-                VideoAndAudio.setPrefWidth(500);
-                VideoAndAudio.setPrefHeight(44);
-                Button Telephone = new Button("Telephone");
-                Telephone.setPrefWidth(500);
-                Telephone.setPrefHeight(44);
+                Button MobileAndTablet = makeButton("MobileAndTablet");
+
+                Button PC = makeButton("PC");
+
+                Button Console = makeButton("Console");
+
+                Button VideoAndAudio = makeButton("VideoAndAudio");
+
+                Button Telephone = makeButton("Telephone");
+
                 vBox.getChildren().addAll(MobileAndTablet, PC, Console, VideoAndAudio, Telephone);
                 break;
+
             case "Home Goods":
-                Button ElectricalAppliance = new Button("ElectricalAppliance");
-                ElectricalAppliance.setPrefWidth(500);
-                ElectricalAppliance.setPrefHeight(44);
-                Button kitchenware = new Button("kitchenware");
-                kitchenware.setPrefWidth(500);
-                kitchenware.setPrefHeight(44);
-                Button FoodAndDrink = new Button("FoodAndDrink");
-                FoodAndDrink.setPrefWidth(500);
-                FoodAndDrink.setPrefHeight(44);
-                Button SewingAccessories = new Button("SewingAccessories");
-                SewingAccessories.setPrefWidth(500);
-                SewingAccessories.setPrefHeight(44);
-                Button Sofa = new Button("Sofa");
-                Sofa.setPrefWidth(500);
-                Sofa.setPrefHeight(44);
-                Button Lighting = new Button("Lighting");
-                Lighting.setPrefWidth(500);
-                Lighting.setPrefHeight(44);
-                Button Carpet = new Button("Carpet");
-                Carpet.setPrefWidth(500);
-                Carpet.setPrefHeight(44);
-                Button Mattress = new Button("Mattress");
-                Mattress.setPrefWidth(500);
-                Mattress.setPrefHeight(44);
-                Button DecorativeAccessories = new Button("DecorativeAccessories");
-                DecorativeAccessories.setPrefWidth(500);
-                DecorativeAccessories.setPrefHeight(44);
-                Button VentilationAppliances = new Button("VentilationAppliances");
-                VentilationAppliances.setPrefWidth(500);
-                VentilationAppliances.setPrefHeight(44);
-                Button WashingSupplies = new Button("WashingSupplies");
-                WashingSupplies.setPrefWidth(500);
-                WashingSupplies.setPrefHeight(44);
-                Button BathroomUtensils = new Button("BathroomUtensils");
-                BathroomUtensils.setPrefWidth(500);
-                BathroomUtensils.setPrefHeight(44);
+                Button ElectricalAppliance = makeButton("ElectricalAppliance");
+
+                Button kitchenware = makeButton("kitchenware");
+
+                Button FoodAndDrink = makeButton("FoodAndDrink");
+
+                Button SewingAccessories = makeButton("SewingAccessories");
+
+                Button Sofa = makeButton("Sofa");
+
+                Button Lighting = makeButton("Lighting");
+
+                Button Carpet = makeButton("Carpet");
+
+                Button Mattress = makeButton("Mattress");
+
+                Button DecorativeAccessories = makeButton("DecorativeAccessories");
+
+                Button VentilationAppliances = makeButton("VentilationAppliances");
+
+                Button WashingSupplies = makeButton("WashingSupplies");
+
+                Button BathroomUtensils = makeButton("BathroomUtensils");
+
                 vBox.getChildren().addAll(ElectricalAppliance, kitchenware, FoodAndDrink, SewingAccessories, Sofa, Lighting, Carpet, Mattress, DecorativeAccessories, VentilationAppliances, WashingSupplies, BathroomUtensils);
                 break;
+
             case "Services":
-                Button EngineAndMachine = new Button("EngineAndMachine");
-                EngineAndMachine.setPrefWidth(500);
-                EngineAndMachine.setPrefHeight(44);
-                Button ReceptionAndCeremony = new Button("ReceptionAndCeremony");
-                ReceptionAndCeremony.setPrefWidth(500);
-                ReceptionAndCeremony.setPrefHeight(44);
-                Button ComputerAndMobileServices = new Button("ComputerAndMobileServices");
-                ComputerAndMobileServices.setPrefWidth(500);
-                ComputerAndMobileServices.setPrefHeight(44);
-                Button FinanceAndAccountingAndInsurance = new Button("FinanceAndAccountingAndInsurance");
-                FinanceAndAccountingAndInsurance.setPrefWidth(500);
-                FinanceAndAccountingAndInsurance.setPrefHeight(44);
-                Button Transportation = new Button("Transportation");
-                Transportation.setPrefWidth(500);
-                Transportation.setPrefHeight(44);
-                Button ProfessionAndSkill = new Button("ProfessionAndSkill");
-                ProfessionAndSkill.setPrefWidth(500);
-                ProfessionAndSkill.setPrefHeight(44);
-                Button HairdressingAndBeauty = new Button("HairdressingAndBeauty");
-                HairdressingAndBeauty.setPrefWidth(500);
-                HairdressingAndBeauty.setPrefHeight(44);
-                Button entertainment = new Button("entertainment");
-                entertainment.setPrefWidth(500);
-                entertainment.setPrefHeight(44);
-                Button cleaning = new Button("cleaning");
-                cleaning.setPrefWidth(500);
-                cleaning.setPrefHeight(44);
-                Button GardeningAndTreePlanting = new Button("GardeningAndTreePlanting");
-                GardeningAndTreePlanting.setPrefWidth(500);
-                GardeningAndTreePlanting.setPrefHeight(44);
-                Button educational = new Button("educational");
-                educational.setPrefWidth(500);
-                educational.setPrefHeight(44);
+                Button EngineAndMachine = makeButton("EngineAndMachine");
+
+                Button ReceptionAndCeremony = makeButton("ReceptionAndCeremony");
+
+                Button ComputerAndMobileServices = makeButton("ComputerAndMobileServices");
+
+                Button FinanceAndAccountingAndInsurance = makeButton("FinanceAndAccountingAndInsurance");
+
+                Button Transportation = makeButton("Transportation");
+
+                Button ProfessionAndSkill = makeButton("ProfessionAndSkill");
+
+                Button HairdressingAndBeauty = makeButton("HairdressingAndBeauty");
+
+                Button entertainment = makeButton("entertainment");
+
+                Button cleaning = makeButton("cleaning");
+
+                Button GardeningAndTreePlanting = makeButton("GardeningAndTreePlanting");
+
+                Button educational = makeButton("educational");
+
                 vBox.getChildren().addAll(EngineAndMachine, ReceptionAndCeremony, ComputerAndMobileServices, FinanceAndAccountingAndInsurance, Transportation, ProfessionAndSkill, HairdressingAndBeauty, entertainment, cleaning, GardeningAndTreePlanting, educational);
                 break;
+
             case "Personal Items":
-                Button BagsAndShoes = new Button("BagsAndShoes");
-                BagsAndShoes.setPrefWidth(500);
-                BagsAndShoes.setPrefHeight(44);
-                Button Accessories = new Button("Accessories");
-                Accessories.setPrefWidth(500);
-                Accessories.setPrefHeight(44);
-                Button Cosmetics = new Button("Cosmetics");
-                Cosmetics.setPrefWidth(500);
-                Cosmetics.setPrefHeight(44);
-                Button KidsClothes = new Button("KidsClothes");
-                KidsClothes.setPrefWidth(500);
-                KidsClothes.setPrefHeight(44);
-                Button Toy = new Button("Toy");
-                Toy.setPrefWidth(500);
-                Toy.setPrefHeight(44);
+                Button BagsAndShoes = makeButton("BagsAndShoes");
+
+                Button Accessories = makeButton("Accessories");
+
+                Button Cosmetics = makeButton("Cosmetics");
+
+                Button KidsClothes = makeButton("KidsClothes");
+
+                Button Toy = makeButton("Toy");
+
                 vBox.getChildren().addAll(BagsAndShoes, Accessories, Cosmetics, KidsClothes, Toy);
                 break;
+
             case "Entertainment":
-                Button Ticket = new Button("Ticket");
-                Ticket.setPrefWidth(500);
-                Ticket.setPrefHeight(44);
-                Button Tour = new Button("Tour");
-                Tour.setPrefWidth(500);
-                Tour.setPrefHeight(44);
-                Button Book = new Button("Book");
-                Book.setPrefWidth(500);
-                Book.setPrefHeight(44);
-                Button Bike = new Button("Bike");
-                Bike.setPrefWidth(500);
-                Bike.setPrefHeight(44);
-                Button Animals = new Button("Animals");
-                Animals.setPrefWidth(500);
-                Animals.setPrefHeight(44);
-                Button Collection = new Button("Collection");
-                Collection.setPrefWidth(500);
-                Collection.setPrefHeight(44);
-                Button MusicInstrument = new Button("MusicInstrument");
-                MusicInstrument.setPrefWidth(500);
-                MusicInstrument.setPrefHeight(44);
-                Button Sport = new Button("Sport");
-                Sport.setPrefWidth(500);
-                Sport.setPrefHeight(44);
+                Button Ticket = makeButton("Ticket");
+
+                Button Tour = makeButton("Tour");
+
+                Button Book = makeButton("Book");
+
+                Button Bike = makeButton("Bike");
+
+                Button Animals = makeButton("Animals");
+
+                Button Collection = makeButton("Collection");
+
+                Button MusicInstrument = makeButton("MusicInstrument");
+
+                Button Sport = makeButton("Sport");
+
                 vBox.getChildren().addAll(Ticket, Tour, Book, Bike, Animals, Collection, MusicInstrument, Sport);
                 break;
+
             case "Social":
-                Button Event = new Button("Event");
-                Event.setPrefWidth(500);
-                Event.setPrefHeight(44);
-                Button Voluntarily = new Button("Voluntarily");
-                Voluntarily.setPrefWidth(500);
-                Voluntarily.setPrefHeight(44);
-                Button Losts = new Button("Losts");
-                Losts.setPrefWidth(500);
-                Losts.setPrefHeight(44);
+                Button Event = makeButton("Event");
+
+                Button Voluntarily = makeButton("Voluntarily");
+
+                Button Losts = makeButton("Losts");
+
                 vBox.getChildren().addAll(Event, Voluntarily, Losts);
                 break;
+
             case "Equipment":
-                Button BuildingMaterials = new Button("BuildingMaterials");
-                BuildingMaterials.setPrefWidth(500);
-                BuildingMaterials.setPrefHeight(44);
-                Button Tools = new Button("Tools");
-                Tools.setPrefWidth(500);
-                Tools.setPrefHeight(44);
-                Button IndustrialMachinery = new Button("IndustrialMachinery");
-                IndustrialMachinery.setPrefWidth(500);
-                IndustrialMachinery.setPrefHeight(44);
-                Button BusinessEquipment = new Button("BusinessEquipment");
-                BusinessEquipment.setPrefWidth(500);
-                BusinessEquipment.setPrefHeight(44);
-                Button Wholesale = new Button("Wholesale");
-                Wholesale.setPrefWidth(500);
-                Wholesale.setPrefHeight(44);
+                Button BuildingMaterials = makeButton("BuildingMaterials");
+
+                Button Tools = makeButton("Tools");
+
+                Button IndustrialMachinery = makeButton("IndustrialMachinery");
+
+                Button BusinessEquipment = makeButton("BusinessEquipment");
+
+                Button Wholesale = makeButton("Wholesale");
+
                 vBox.getChildren().addAll(BuildingMaterials, Tools, IndustrialMachinery, BusinessEquipment, Wholesale);
                 break;
+
             case "Recruitment":
-                Button Management = new Button("Management");
-                Management.setPrefWidth(500);
-                Management.setPrefHeight(44);
-                Button Caretaker = new Button("Caretaker");
-                Caretaker.setPrefWidth(500);
-                Caretaker.setPrefHeight(44);
-                Button Architect = new Button("Architect");
-                Architect.setPrefWidth(500);
-                Architect.setPrefHeight(44);
-                Button StoreServices = new Button("StoreServices");
-                StoreServices.setPrefWidth(500);
-                StoreServices.setPrefHeight(44);
-                Button ComputerAndIT = new Button("ComputerAndIT");
-                ComputerAndIT.setPrefWidth(500);
-                ComputerAndIT.setPrefHeight(44);
-                Button Accountants = new Button("Accountants");
-                Accountants.setPrefWidth(500);
-                Accountants.setPrefHeight(44);
-                Button Marketer = new Button("Marketer");
-                Marketer.setPrefWidth(500);
-                Marketer.setPrefHeight(44);
-                Button Engineer = new Button("Engineer");
-                Engineer.setPrefWidth(500);
-                Engineer.setPrefHeight(44);
-                Button Teacher = new Button("Teacher");
-                Teacher.setPrefWidth(500);
-                Teacher.setPrefHeight(44);
-                Button Driver = new Button("Driver");
-                Driver.setPrefWidth(500);
-                Driver.setPrefHeight(44);
-                Button DoctorAndBeautician = new Button("DoctorAndBeautician");
-                DoctorAndBeautician.setPrefWidth(500);
-                DoctorAndBeautician.setPrefHeight(44);
-                Button Artist = new Button("Artist");
-                Artist.setPrefWidth(500);
-                Artist.setPrefHeight(44);
+                Button Management = makeButton("Management");
+
+                Button Caretaker = makeButton("Caretaker");
+
+                Button Architect = makeButton("Architect");
+
+                Button StoreServices = makeButton("StoreServices");
+
+                Button ComputerAndIT = makeButton("ComputerAndIT");
+
+                Button Accountants = makeButton("Accountants");
+
+                Button Marketer = makeButton("Marketer");
+
+                Button Engineer = makeButton("Engineer");
+
+                Button Teacher = makeButton("Teacher");
+
+                Button Driver = makeButton("Driver");
+
+                Button DoctorAndBeautician = makeButton("DoctorAndBeautician");
+
+                Button Artist = makeButton("Artist");
+
                 vBox.getChildren().addAll(Management, Caretaker, Architect, StoreServices, ComputerAndIT, Accountants, Marketer, Engineer, Teacher, Driver, DoctorAndBeautician, Artist);
                 break;
         }
     }
 
-
-
+    private Button makeButton(String Name) {
+        Button button = new Button(Name);
+        button.setPrefWidth(500);
+        button.setPrefHeight(44);
+        button.setOnAction(event -> {
+            PageThreeCategories pageThreeCategories = new PageThreeCategories(mainVbox,Name);
+        });
+        return button;
+    }
 }
