bb3391ccfd54a7a0db0b573a985455f0b5a301cb
Abolfazl <abolfazl30not30@gmail.com>
2022-06-25 16:40:20 +0430

    make chatSection

--- Client/src/main/java/com/example/client/Dashboard/ChatController.java
+++ Client/src/main/java/com/example/client/Dashboard/ChatController.java
@@ -1,20 +1,77 @@
 package com.example.client.Dashboard;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
+import org.json.JSONObject;
 
 public class ChatController {
 
     @FXML
     private Circle circleImage;
+    @FXML
+    private VBox Users;
+    @FXML
+    private Circle imageProfile;
+    @FXML
+    private Label nameProfile;
+    @FXML
+    private VBox chatBox;
+    @FXML
+    private TextField textBox;
+    @FXML
+    private Button sendButton;
 
     @FXML
     public void initialize(){
-        Image img = new Image("/postImage/1.jpg");
-        circleImage.setFill(new ImagePattern(img));
+        for (int i = 0; i < 10; i++) {
+            HBox hBox = AddUsers();
+            Users.getChildren().add(hBox);
+        }
+    }
+
+    @FXML
+    private void sendButton(){
 
     }
 
+    private HBox AddUsers(){
+        HBox userHBox = new HBox();
+        userHBox.setAlignment(Pos.CENTER_LEFT);
+        userHBox.setMinHeight(56);
+        userHBox.setMinWidth(194);
+        userHBox.setPrefHeight(56);
+        userHBox.setPrefWidth(56);
+
+        Circle userImg = new Circle();
+        userImg.setRadius(24);
+        Image img = new Image("/postImage/1.jpg");
+        userImg.setFill(new ImagePattern(img));
+
+        Label userName = new Label("Ali Mohammadi");
+        userName.setAlignment(Pos.CENTER);
+        userName.setPrefWidth(114);
+        userName.setPrefHeight(52);
+
+        Label counterOfMassages = new Label("23");
+        counterOfMassages.setAlignment(Pos.CENTER);
+        counterOfMassages.setPrefWidth(25);
+        counterOfMassages.setPrefHeight(52);
+        userHBox.getChildren().addAll(userImg,userName,counterOfMassages);
+        userHBox.setOnMouseClicked(event -> ShowMassages());
+        return userHBox;
+
+    }
+
+    private void ShowMassages(){
+        Label label = new Label("helllllooo");
+        chatBox.getChildren().add(label);
+    }
 }
