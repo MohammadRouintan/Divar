23e7109f5924142ff227849e6216c1f658bd1117
Abolfazl <abolfazl30not30@gmail.com>
2022-06-26 18:07:49 +0430

    design Apply button for filter

--- Client/src/main/java/com/example/client/Dashboard/AdsController.java
+++ Client/src/main/java/com/example/client/Dashboard/AdsController.java
@@ -36,7 +36,7 @@ public class AdsController {
 
     }
     private void setMainCategories(){
-        String[] mainCategories = {"Digital Goods", "Entertainment", "Equipment", "Estate", "Home Goods", "Personal Items", "Recruitment", "Services", "Social", "Vehicles"};
+        String[] mainCategories = {"","Digital Goods", "Entertainment", "Equipment", "Estate", "Home Goods", "Personal Items", "Recruitment", "Services", "Social", "Vehicles"};
         ObservableList<String> temp = FXCollections.observableArrayList();
         temp.addAll(mainCategories);
         mainBranchCategories.setItems(temp);
@@ -48,6 +48,7 @@ public class AdsController {
         ObservableList<String> temp = FXCollections.observableArrayList();
         temp.addAll(cityes);
         City.setItems(temp);
+        City.getSelectionModel().select(0);
     }
 
     @FXML
@@ -57,16 +58,16 @@ public class AdsController {
 
     public void setBranchTwoCategories() {
         ObservableList<String> branchTwo = FXCollections.observableArrayList();
-        String[] estate = {"ResidentialSales", "ResidentialRent", "OfficeSales", "OfficeRent", "ShortTermRent", "ConstructionProject"};
-        String[] vehicles = {"Car", "CarSparePartsAndAccessories", "MotorcyclesAndAccessories", "BoatsAndAccessories"};
-        String[] digitalCommodity = {"MobileAndTablet", "PC", "Console", "VideoAndAudio", "Telephone"};
-        String[] homeCommodity = {"ElectricalAppliance", "kitchenware", "FoodAndDrink", "SewingAccessories", "Sofa", "Lighting", "Carpet", "Mattress", "DecorativeAccessories", "VentilationAppliances", "WashingSupplies", "BathroomUtensils"};
-        String[] services = {"EngineAndMachine", "ReceptionAndCeremony", "ComputerAndMobileServices", "FinanceAndAccountingAndInsurance", "Transportation", "ProfessionAndSkill", "HairdressingAndBeauty", "entertainment", "cleaning", "GardeningAndTreePlanting", "educational"};
-        String[] personalItems = {"BagsAndShoes", "Accessories", "Cosmetics", "KidsClothes", "Toy"};
-        String[] entertainment = {"Ticket", "Tour", "Book", "Bike", "Animals", "Collection", "MusicInstrument", "Sport"};
-        String[] social = {"Event", "Voluntarily", "Losts"};
-        String[] equipment = {"BuildingMaterials", "Tools", "IndustrialMachinery", "BusinessEquipment", "Wholesale"};
-        String[] recruitment = {"Management", "Caretaker", "Architect", "StoreServices", "ComputerAndIT", "Accountants", "Marketer", "Engineer", "Teacher", "Driver", "DoctorAndBeautician", "Artist"};
+        String[] estate = {"","ResidentialSales", "ResidentialRent", "OfficeSales", "OfficeRent", "ShortTermRent", "ConstructionProject"};
+        String[] vehicles = {"","Car", "CarSparePartsAndAccessories", "MotorcyclesAndAccessories", "BoatsAndAccessories"};
+        String[] digitalCommodity = {"","MobileAndTablet", "PC", "Console", "VideoAndAudio", "Telephone"};
+        String[] homeCommodity = {"","ElectricalAppliance", "kitchenware", "FoodAndDrink", "SewingAccessories", "Sofa", "Lighting", "Carpet", "Mattress", "DecorativeAccessories", "VentilationAppliances", "WashingSupplies", "BathroomUtensils"};
+        String[] services = {"","EngineAndMachine", "ReceptionAndCeremony", "ComputerAndMobileServices", "FinanceAndAccountingAndInsurance", "Transportation", "ProfessionAndSkill", "HairdressingAndBeauty", "entertainment", "cleaning", "GardeningAndTreePlanting", "educational"};
+        String[] personalItems = {"","BagsAndShoes", "Accessories", "Cosmetics", "KidsClothes", "Toy"};
+        String[] entertainment = {"","Ticket", "Tour", "Book", "Bike", "Animals", "Collection", "MusicInstrument", "Sport"};
+        String[] social = {"","Event", "Voluntarily", "Losts"};
+        String[] equipment = {"","BuildingMaterials", "Tools", "IndustrialMachinery", "BusinessEquipment", "Wholesale"};
+        String[] recruitment = {"","Management", "Caretaker", "Architect", "StoreServices", "ComputerAndIT", "Accountants", "Marketer", "Engineer", "Teacher", "Driver", "DoctorAndBeautician", "Artist"};
 
         String mainBranch = mainBranchCategories.getValue();
         switch (mainBranch) {
@@ -76,10 +77,10 @@ public class AdsController {
             case "Vehicles":
                 branchTwo.addAll(vehicles);
                 break;
-            case "Digital Commodity":
+            case "Digital Goods":
                 branchTwo.addAll(digitalCommodity);
                 break;
-            case "Home Commodity":
+            case "Home Goods":
                 branchTwo.addAll(homeCommodity);
                 break;
             case "Services":
