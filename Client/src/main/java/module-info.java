8b30649f0e524246122caa3ba66d35830a5ea686
Abolfazl <abolfazl30not30@gmail.com>
2022-06-27 12:07:13 +0430

    fix bug

--- Client/src/main/java/module-info.java
+++ Client/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.client {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.json;
-
+    requires com.jfoenix;
 
     opens com.example.client to javafx.fxml;
     exports com.example.client;
