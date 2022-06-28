572b66325f6005d8bf180c479950c9f3ac4e31a1
Mohammad Rouintan <mdrn1381@gmail.com>
2022-06-28 06:36:24 +0430

    set socket for getProfileId

--- Client/src/main/java/com/example/client/socket/GetInfo.java
+++ Client/src/main/java/com/example/client/socket/GetInfo.java
@@ -502,16 +502,16 @@ public class GetInfo {
     }
 
     public static int getProfileID(String userPhone) {
-        int res = 0;
+        int result = 0;
         try {
-            Connect.DOS.writeInt(22);
+            Connect.DOS.writeInt(27);
             Connect.DOS.flush();
             Connect.DOS.writeUTF(userPhone);
             Connect.DOS.flush();
-            res = Connect.DIS.readInt();
+            result = Connect.DIS.readInt();
         } catch (IOException e){
             System.err.println(e.getMessage());
         }
-        return res;
+        return result;
     }
 }
