1e427b1550655f17c4574bcb51cef6d756589927
Mohammad Rouintan <mdrn1381@gmail.com>
2022-07-05 19:28:35 +0430

    fixing ads controller and ads section

--- Client/src/main/java/com/example/client/Dashboard/AdsController.java
+++ Client/src/main/java/com/example/client/Dashboard/AdsController.java
@@ -1,93 +1,79 @@
 package com.example.client.Dashboard;
 
-        import com.example.client.Dashboard.Posts.NewPage;
-        import com.example.client.socket.GetInfo;
-
-        import javafx.collections.FXCollections;
-        import javafx.collections.ObservableList;
-        import javafx.event.ActionEvent;
-        import javafx.fxml.FXML;
-        import javafx.scene.Node;
-        import javafx.scene.control.ComboBox;
-        import javafx.scene.control.Pagination;
-        import javafx.scene.control.TextField;
-        import javafx.scene.layout.HBox;
-        import javafx.scene.layout.VBox;
-        import javafx.util.Duration;
-        import org.json.JSONObject;
-
-        import java.time.LocalDateTime;
-        import java.util.ArrayList;
-        import java.util.List;
+import com.example.client.Dashboard.Posts.NewPage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Pagination;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class AdsController {
-    public ComboBox<String> City1;
-    public Pagination pagination1;
+
+    // Home
     @FXML
-    private HBox searchHbox;
+    public Tab home;
     @FXML
-    private ComboBox<String> City;
-    @FXML
-    private TextField searchTextbox;
+    private ComboBox<String> homeTabCity;
     @FXML
     private ComboBox<String> mainBranchCategories;
     @FXML
     private ComboBox<String> branchTwoCategories;
     @FXML
+    private TextField searchText;
+    @FXML
     private TextField priceFrom;
     @FXML
     private TextField priceTo;
     @FXML
-    private Pagination pagination;
+    private Pagination homePagination;
 
-    private List<VBox> PagesList = new ArrayList<VBox>();
+    // Recommended
+    @FXML
+    public Tab recommended;
+    @FXML
+    public ComboBox<String> recommendedTabCity;
+    @FXML
+    public Pagination recommendedPagination;
+
+    private List<VBox> PagesList = new ArrayList<>();
     private ArrayList<String> keys = new ArrayList<>();
     private ArrayList<Object> values =  new ArrayList<>();
-    private ArrayList<String> list = new ArrayList<>();
 
     @FXML
-    public void initialize(){
-        setMainCategories();
-        setCity();
-        ArrayList<JSONObject> post = new ArrayList<>();
-        ArrayList<String> imageName = new ArrayList<>();
-        imageName.add("1");
-        imageName.add("2");
-        ArrayList<String> columnName = new ArrayList<>();
-        columnName.add("Salam");
-        columnName.add("Khobi");
-
-        ArrayList<String> columnValue = new ArrayList<>();
-        columnValue.add("dddddd");
-        columnValue.add("bbbbbb");
-
-        for (int i = 0; i < 7; i++) {
-            JSONObject post1 = new JSONObject();
-            post1.put("title", "salam");
-            post1.put("price", "14000");
-            post1.put("time", "5");
-            post1.put("image1", "/postImage/1.jpg");
-            post1.put("imageName",imageName);
-            post1.put("columnName",columnName);
-            post1.put("columnValue",columnValue);
-            post1.put("bio","I have a scene showing 3 images, and I want each of them to take a third of the width of the scene. " +
-                    "From now, I have made 3 Pane of each 30% of it, it works. But in those Pane," +
-                    " I can't make my ImageView use only the width of the Pane.");
-            post1.put("agreement", true);
-            post1.put("exchange", true);
-            post1.put("auction", false);
-            post.add(post1);
+    public void initialize(String tab) {
+        if (tab.equals("home")) {
+            setCity("homeTabCity");
+            setMainCategories();
+            mainBranchCategories.getSelectionModel().select("");
+            branchTwoCategories.getSelectionModel().select("");
+            homeTabCity.getSelectionModel().select("Tehran");
+            keys.add("city");
+            values.add("Tehran");
+            //int size = (int) Math.ceil((double) GetInfo.getSizeOfPosts(keys, values) / 8);
+            homePagination.setPageCount(1);
+            homePagination.setMaxPageIndicatorCount(2);
+            homePagination.setPageFactory(this::CreatePage);
+        } else if (tab.equals("recommended")) {
+            setCity("recommendedTabCity");
+            recommendedTabCity.getSelectionModel().select("Tehran");
+            keys.add("city");
+            values.add("Tehran");
+            //int size = (int) Math.ceil((double) GetInfo.getSizeOfPosts(keys, values) / 8);
+            recommendedPagination.setPageCount(1);
+            recommendedPagination.setMaxPageIndicatorCount(2);
+            recommendedPagination.setPageFactory(this::CreatePage);
         }
-        mainBranchCategories.getSelectionModel().select("");
-        branchTwoCategories.getSelectionModel().select("");
-        City.getSelectionModel().select("Tehran");
-        keys.add("city");
-        values.add("Tehran");
-        //int size = (int) Math.ceil((double) GetInfo.getSizeOfPosts(keys, values) / 8);
-        pagination.setPageCount(1);
-        pagination.setMaxPageIndicatorCount(2);
-        pagination.setPageFactory(this::CreatePage);
     }
 
     private Node CreatePage(int pageIndex) {
@@ -95,10 +81,10 @@ public class AdsController {
         ArrayList<String> imageName = new ArrayList<>();
         imageName.add("1");
         imageName.add("2");
+
         ArrayList<String> columnName = new ArrayList<>();
         columnName.add("Salam");
         columnName.add("Khobi");
-
         ArrayList<String> columnValue = new ArrayList<>();
         columnValue.add("dddddd");
         columnValue.add("bbbbbb");
@@ -124,24 +110,32 @@ public class AdsController {
         return PagesList.get(pageIndex);
     }
 
-    private void setMainCategories(){
-        String[] mainCategories = {"","Digital Goods", "Entertainment", "Equipment", "Estate", "Home Goods", "Personal Items", "Recruitment", "Services", "Social", "Vehicles"};
+    private void setMainCategories() {
+        String[] mainCategories = {"", "Digital Goods", "Entertainment", "Equipment", "Estate", "Home Goods", "Personal Items", "Recruitment", "Services", "Social", "Vehicles"};
         ObservableList<String> temp = FXCollections.observableArrayList();
         temp.addAll(mainCategories);
         mainBranchCategories.setItems(temp);
     }
 
-    private void setCity(){
-        String[] cityes = {"Tehran" ,"Shiraz" ,"Mashhad" ,"Arak" ,"Ardabil" ,"Orumieh" ,"Esfahan" ,"Ahwaz" ,"Ilam" ," Bojnord" ,
-                "Bandar Abbas" ,"Bushehr" ,"Birjand" ,"Tabriz" ,"Khorramabad" ,"Rasht" ,"Zahedan" ,"Zanjan" ,"Sari" ,"Semnan" ,"Sanandaj" ,"Shahr e Kord" ,"Qazvin" ,"Qom" ,"Karaj" ,"Kerman" ,"Kermanshah" ,"Gorgan" ,"Hamedan" ,"Yasuj" ,"Yazd"};
+    private void setCity(String city) {
+        String[] cities = {"Tehran", "Shiraz", "Mashhad", "Arak", "Ardabil", "Orumieh", "Esfahan", "Ahwaz", "Ilam", " Bojnord",
+                "Bandar Abbas", "Bushehr", "Birjand", "Tabriz", "Khorramabad", "Rasht", "Zahedan", "Zanjan", "Sari", "Semnan",
+                "Sanandaj", "Shahr e Kord", "Qazvin", "Qom", "Karaj", "Kerman", "Kermanshah", "Gorgan", "Hamedan", "Yasuj", "Yazd"};
+
         ObservableList<String> temp = FXCollections.observableArrayList();
-        temp.addAll(cityes);
-        City.setItems(temp);
-        City.getSelectionModel().select(0);
+        temp.addAll(cities);
+
+        if (city.equals("homeTabCity")) {
+            this.homeTabCity.setItems(temp);
+            this.homeTabCity.getSelectionModel().select(0);
+        } else if (city.equals("recommendedTabCity")) {
+            recommendedTabCity.setItems(temp);
+            recommendedTabCity.getSelectionModel().select(0);
+        }
     }
 
     @FXML
-    void MainBranchCategotiesFunction(ActionEvent event) {
+    void MainBranchCategoriesFunction(ActionEvent event) {
         setBranchTwoCategories();
     }
 
@@ -159,65 +153,71 @@ public class AdsController {
         String[] recruitment = {"","Management", "Caretaker", "Architect", "StoreServices", "ComputerAndIT", "Accountants", "Marketer", "Engineer", "Teacher", "Driver", "DoctorAndBeautician", "Artist"};
 
         String mainBranch = mainBranchCategories.getValue();
-        switch (mainBranch) {
-            case "Estate":
-                branchTwo.addAll(estate);
-                break;
-            case "Vehicles":
-                branchTwo.addAll(vehicles);
-                break;
-            case "Digital Goods":
-                branchTwo.addAll(digitalCommodity);
-                break;
-            case "Home Goods":
-                branchTwo.addAll(homeCommodity);
-                break;
-            case "Services":
-                branchTwo.addAll(services);
-                break;
-            case "Personal Items":
-                branchTwo.addAll(personalItems);
-                break;
-            case "Entertainment":
-                branchTwo.addAll(entertainment);
-                break;
-            case "Social":
-                branchTwo.addAll(social);
-                break;
-            case "Equipment":
-                branchTwo.addAll(equipment);
-                break;
-            case "Recruitment":
-                branchTwo.addAll(recruitment);
-                break;
-            default:
-                break;
+        if (mainBranch != null) {
+            switch (mainBranch) {
+                case "Estate":
+                    branchTwo.addAll(estate);
+                    break;
+                case "Vehicles":
+                    branchTwo.addAll(vehicles);
+                    break;
+                case "Digital Goods":
+                    branchTwo.addAll(digitalCommodity);
+                    break;
+                case "Home Goods":
+                    branchTwo.addAll(homeCommodity);
+                    break;
+                case "Services":
+                    branchTwo.addAll(services);
+                    break;
+                case "Personal Items":
+                    branchTwo.addAll(personalItems);
+                    break;
+                case "Entertainment":
+                    branchTwo.addAll(entertainment);
+                    break;
+                case "Social":
+                    branchTwo.addAll(social);
+                    break;
+                case "Equipment":
+                    branchTwo.addAll(equipment);
+                    break;
+                case "Recruitment":
+                    branchTwo.addAll(recruitment);
+                    break;
+                default:
+                    branchTwo.add("");
+                    break;
+            }
         }
 
         branchTwoCategories.setItems(branchTwo);
     }
 
     @FXML
-    void searchButton(){
+    void searchButton() {
         PagesList.clear();
         keys.clear();
         values.clear();
         keys.add("city");
-        values.add(City.getValue());
+        values.add(homeTabCity.getValue());
         priceFrom.clear();
         priceTo.clear();
         //int size = (int) Math.ceil((double) GetInfo.getSizeOfPosts(keys, values) / 8);
-        pagination.setPageCount(1);
-        pagination.setMaxPageIndicatorCount(2);
-        pagination.setPageFactory(this::CreatePage);
+        homePagination.setPageCount(1);
+        homePagination.setMaxPageIndicatorCount(2);
+        homePagination.setPageFactory(this::CreatePage);
     }
 
     private ArrayList<String> search(ArrayList<String> post) {
         JSONObject jsonObject;
+        String str1, str2;
         ArrayList<String> temp = new ArrayList<>();
         for (int i = 0; i < post.size(); i++) {
             jsonObject = new JSONObject(post.get(i));
-            if (jsonObject.getString("title").contains(searchTextbox.getText())) {
+            str1 = jsonObject.getString("title").toLowerCase();
+            str2 = searchText.getText().toLowerCase();
+            if (str1.contains(str2)) {
                 temp.add(post.get(i));
             }
         }
@@ -231,7 +231,7 @@ public class AdsController {
         keys.clear();
         values.clear();
         keys.add("city");
-        values.add(City.getValue());
+        values.add(homeTabCity.getValue());
         if (!mainBranchCategories.getValue().equals("") && !branchTwoCategories.getValue().equals("")) {
             keys.add("branchMain");
             values.add(mainBranchCategories.getValue());
@@ -242,11 +242,10 @@ public class AdsController {
             values.add(mainBranchCategories.getValue());
         }
 
-
         //int size = (int) Math.ceil((double) GetInfo.getSizeOfPosts(keys, values) / 8);
-        pagination.setPageCount(1);
-        pagination.setMaxPageIndicatorCount(2);
-        pagination.setPageFactory(this::CreatePage);
+        homePagination.setPageCount(1);
+        homePagination.setMaxPageIndicatorCount(2);
+        homePagination.setPageFactory(this::CreatePage);
     }
 
     @FXML
@@ -255,21 +254,40 @@ public class AdsController {
         keys.clear();
         values.clear();
         keys.add("city");
-        values.add(City.getValue());
-        searchTextbox.clear();
+        values.add(homeTabCity.getValue());
+        searchText.clear();
 
         //int size = (int) Math.ceil((double) GetInfo.getSizeOfPosts(keys, values) / 8);
-        pagination.setPageCount(1);
-        pagination.setMaxPageIndicatorCount(2);
-        pagination.setPageFactory(this::CreatePage);
+        homePagination.setPageCount(1);
+        homePagination.setMaxPageIndicatorCount(2);
+        homePagination.setPageFactory(this::CreatePage);
     }
 
     private void priceFilter() {
         if (!priceFrom.getText().equals("") && !priceTo.getText().equals("")) {
-            //list = GetInfo.priceFilter(Long.parseLong(priceFrom.getText()), Long.parseLong(priceTo.getText()), list);
+            list = GetInfo.priceFilter(Long.parseLong(priceFrom.getText()), Long.parseLong(priceTo.getText()), list);
         }
     }
 
+    @FXML
+    void homeButton(Event event) {
+        if (home.isSelected()) {
+            PagesList.clear();
+            keys.clear();
+            values.clear();
+            initialize("home");
+        }
+    }
+
+    @FXML
+    void recommendedButton(Event event) {
+        if (recommended.isSelected()) {
+            PagesList.clear();
+            keys.clear();
+            values.clear();
+            initialize("recommended");
+        }
+    }
 }
 
 
